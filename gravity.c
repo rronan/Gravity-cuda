04eefc416d8d34a67179c888e4d7c3f000f10260
ronan <ronan@milvue.com>
2022-04-18 20:55:11 +0200

    cleaning

--- gravity.c
+++ gravity.c
@@ -1,59 +1,48 @@
 #include <Python.h>
-#include <structmember.h>
-#include <stddef.h>
-#include <math.h>
-#include <unistd.h>
-#include <termios.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #define NPY_NO_DEPRECATED_API NPY_1_8_API_VERSION
 #define PY_ARRAY_UNIQUE_SYMBOL NP_ARRAY_API
-
 #include <numpy/arrayobject.h>
 
 struct Body {
-    double x;
-    double y;
-    double z;
-    double vx;
-    double vy;
-    double vz;
+    double* x;
+    double* y;
+    double* z;
+    double* vx;
+    double* vy;
+    double* vz;
 };
 
 void setSpace(struct Body *bodies[], PyArrayObject *space_arr, unsigned long nbodies){
     for (unsigned long i=0;i<nbodies;i++){
         bodies[i] = (struct Body*)malloc(sizeof(struct Body));
-        bodies[i]->x = *((double*)PyArray_GETPTR3(space_arr,i,0,0));
-        bodies[i]->y = *((double*)PyArray_GETPTR3(space_arr,i,1,0));
-        bodies[i]->z = *((double*)PyArray_GETPTR3(space_arr,i,2,0));
-        bodies[i]->vx = *((double*)PyArray_GETPTR3(space_arr,i,0,1));
-        bodies[i]->vy = *((double*)PyArray_GETPTR3(space_arr,i,1,1));
-        bodies[i]->vz = *((double*)PyArray_GETPTR3(space_arr,i,2,1));
+        bodies[i]->x = (double*)PyArray_GETPTR3(space_arr,i,0,0);
+        bodies[i]->y = (double*)PyArray_GETPTR3(space_arr,i,1,0);
+        bodies[i]->z = (double*)PyArray_GETPTR3(space_arr,i,2,0);
+        bodies[i]->vx = (double*)PyArray_GETPTR3(space_arr,i,0,1);
+        bodies[i]->vy = (double*)PyArray_GETPTR3(space_arr,i,1,1);
+        bodies[i]->vz = (double*)PyArray_GETPTR3(space_arr,i,2,1);
     }
 }
 
 void forwardGravitation(struct Body *a, struct Body *b, double G, double DT, double DAMPING, double SOFTENING) {
-    double px = pow(a->x - b->x, 2);
-    double py = pow(a->y - b->y, 2);
-    double pz = pow(a->z - b->z, 2);
+    double px = pow(*a->x - *b->x, 2);
+    double py = pow(*a->y - *b->y, 2);
+    double pz = pow(*a->z - *b->z, 2);
     double r = pow(px + py + pz + SOFTENING, .5);
     double f = G / (r * r);
-    a->vx = (a->vx + DT * f * (b->x - a->x) / r) * DAMPING;
-    a->vy = (a->vy + DT * f * (b->y - a->y) / r) * DAMPING;
-    a->vz = (a->vz + DT * f * (b->z - a->z) / r) * DAMPING;
+    *a->vx = (*a->vx + DT * f * (*b->x - *a->x) / r) * DAMPING;
+    *a->vy = (*a->vy + DT * f * (*b->y - *a->y) / r) * DAMPING;
+    *a->vz = (*a->vz + DT * f * (*b->z - *a->z) / r) * DAMPING;
 }
 
 void forwardPhysics(struct Body *bodies[], unsigned long nbodies, double G, double DT, double DAMPING, double SOFTENING) {
     for (unsigned long i = 0; i < nbodies; i++) {
         for (unsigned long j = 0; j < nbodies; j++) {
-            if (i != j) {
-                forwardGravitation(bodies[i], bodies[j], G, DT, DAMPING, SOFTENING);
-            }
+            if (i != j) forwardGravitation(bodies[i], bodies[j], G, DT, DAMPING, SOFTENING);
         }
-        bodies[i]->x += DT * bodies[i]->vx;
-        bodies[i]->y += DT * bodies[i]->vy;
-        bodies[i]->z += DT * bodies[i]->vz;
+        *bodies[i]->x += DT * (*bodies[i]->vx);
+        *bodies[i]->y += DT * (*bodies[i]->vy);
+        *bodies[i]->z += DT * (*bodies[i]->vz);
     };
 }
 
@@ -79,9 +68,7 @@ static PyObject * run(PyObject* Py_UNUSED(self), PyObject* args) {
     return Py_None;
 }
 
-static PyMethodDef module_methods[] = {
-    {"run", run, METH_VARARGS, NULL}, {0, 0}
-};
+static PyMethodDef module_methods[] = {{"run", run, METH_VARARGS, NULL}, {0, 0}};
 
 static struct PyModuleDef gravity = {
     PyModuleDef_HEAD_INIT,
@@ -89,8 +76,7 @@ static struct PyModuleDef gravity = {
     .m_methods = module_methods
 };
 
-PyMODINIT_FUNC
-PyInit__gravity(void) {
+PyMODINIT_FUNC PyInit__gravity(void) {
     Py_Initialize();
     import_array();
     return PyModule_Create(&gravity);
